6d9a5b739228ce5a3be189acdebf097c9aab7c1d
guptaneha0111 <nehagupta01113@gmail.com>
2022-07-23 01:02:19 +0530

    added dampfiles

---
+++ checkout.js
@@ -0,0 +1,9 @@
+document.getElementById("ptc").addEventListener("click",payment)
+ function payment(){
+    window.location.href="payment.html";
+ }
+ document.getElementById("backb").addEventListener("click",bagb)
+ function bagb(){
+    window.location.href="cart.html";
+ }
+ 
